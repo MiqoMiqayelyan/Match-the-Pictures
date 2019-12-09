5815f9087496b72cdf6bb5a00cf8b258a9f9aede
MiqoMiqayelyan <miqomiqayelyan97@gmail.com>
2019-12-09 10:55:28 +0400

    update

--- students-task/src/components/studentsList/studentsList.js
+++ students-task/src/components/studentsList/studentsList.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import  DeleteStudentAction  from '../../actions/DeleteStudentAction';
+import UpdateStudentAction from '../../actions/UpdateStudentAction';
+import ageValidation from '../../AgeValidation';
 
 function StudentsList({name, age, prog, id, hideStudent, changeStudentHiding}){
     const [updateName, updateStudentName] = useState({
       name: name,
       valid: true
     });
-    const [updateAge, updateStudentAge] = useState(age);
-    const [updatepPog, updateStudentProg] = useState(prog);
-    
+    const [updateAge, updateStudentAge] = useState({
+      age: age,
+      valid: true
+    });
+    const [updateProg, updateStudentProg] = useState({
+      prog: prog,
+      valid: false
+    });
     const dispatch = useDispatch();
+    
     const editStudentInfo = (e) => {
         e.preventDefault();
         changeStudentHiding({
@@ -20,32 +29,29 @@ function StudentsList({name, age, prog, id, hideStudent, changeStudentHiding}){
     
       const deleteStudent = (e) => {
         e.preventDefault()
-        dispatch({
-            type: 'DELETE_STUDENT',
-            payload: {
-              id: id,
-            }
-          });
+        dispatch(DeleteStudentAction(id));
       }
 
       const updateStudentInfo = (e) => {
         e.preventDefault();
-        if(updateName.name.length > 2){
+        if((updateName.name && updateName.name.length > 2) && (updateProg.prog !== 'None') && ageValidation(updateAge.age)){
           changeStudentHiding({
             hiding: false,
             id: id
           });
-          dispatch({
-              type: 'UPDATE_STUDENT',
-              payload: {
-                id: id,
-                name: updateName.name,
-                age: updateAge,
-                prog: updatepPog
-              }
-          });
-        }else{
-          updateStudentName({valid: false})
+          dispatch(UpdateStudentAction(id, updateName.name, updateAge.age, updateProg.prog));
+        }
+
+        if(!updateName.name || updateName.name.length < 2){
+          updateStudentName({valid: false});
+        }
+
+        if(updateProg.prog === 'None'){
+          updateStudentProg({valid: false});
+        }
+
+        if(updateAge.age === '' && !ageValidation(updateAge.age)){
+          updateAge({valid: false});
         }
             
       }
@@ -55,8 +61,20 @@ function StudentsList({name, age, prog, id, hideStudent, changeStudentHiding}){
            name: event.target.value,
            valid: true
          })} type="text" value={updateName.name}/>
-         <input onChange={event => updateStudentAge(event.target.value)} max="2001-01-01"  type="date" value={updateAge}/>
-         <select onChange={event => updateStudentProg(event.target.value)} value={updatepPog}>
+         <input onChange={event => updateStudentAge({
+           age: event.target.value,
+           valid: ageValidation(event.target.value)
+         })} 
+         className={(updateAge.valid ? '' : 'invalid')}
+         type="date" 
+         value={updateAge.age}/>
+         <select required 
+         onChange={event => updateStudentProg({
+           prog: event.target.value,
+           valid: true
+         })} 
+         className={(updateName.valid ? '' : 'invalid')} value={updateProg.prog}>
+            <option>None</option>
             <option>Wery good</option>
             <option>Good</option>
             <option>Normal</option>
@@ -70,4 +88,6 @@ function StudentsList({name, age, prog, id, hideStudent, changeStudentHiding}){
      </form>
     )
 }
+
+
 export default StudentsList;
